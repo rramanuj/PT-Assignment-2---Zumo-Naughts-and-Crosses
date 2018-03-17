7ee45fa8c74b5e508f511c73d8a10c231f40abbf
Mark Scholefield <marcuthh@gmail.com>
2018-03-17 09:23:15 +0000

    some shit that was on my pc

--- Naughts_and_Crosses/Naughts_and_Crosses.ino
+++ Naughts_and_Crosses/Naughts_and_Crosses.ino
@@ -12,7 +12,7 @@ using namespace std;
 
 // -- GLOBALS -- //
 int crosses_x, crosses_y, naughts_x, naughts_y;
-String crosses_direction, naughts_direction;
+String crosses_facing, naughts_facing;
 bool isNaught;
 // -- CONSTANTS -- //
 //pin values
@@ -63,155 +63,155 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- char inputChar;
- int x, y;
-    while (inputChar != 'X') //this is to end the game
+  char inputChar;
+  int x, y;
+  while (inputChar != 'X') //this is to end the game
+  {
+    if ((Serial.available() > 0) && (Serial.available() < 3))
     {
-      if ((Serial.available() > 0) && (Serial.available() < 3))
-      {
-        message = Serial.read();
-        y = Serial.read();
-        x = Serial.read();
-      }
+      message = Serial.read();
+      y = Serial.read();
+      x = Serial.read();
+    }
 
-      switch (message)
-      {
-        case 'm': case 'M':
+    switch (message)
+    {
+      case 'm': case 'M':
         move(x, y); break;
         // case 'a': case 'A': rotate(fmod(averageHeading() - 93, 360));lastMove = 1;break; //aprox left
         // case 's': case 'S': digitalWrite(LED_PIN, HIGH); motors.setLeftSpeed(-REVERSE_SPEED); motors.setRightSpeed(-REVERSE_SPEED); delay(REVERSE_DURATION); break;
         // case 'd': case 'D': rotate(fmod(averageHeading() + 93, 360));lastMove = 0;break; //approx right
-      }
-}
-void move(int x, int y)
-{
-  int movesX = 0, movesY = 0;
-  if (isNaught) {
+    }
+  }
+  void move(int x, int y)
+  {
+    int movesX = 0, movesY = 0;
+    if (isNaught) {
       movesX = (x - naughts_x);
       movesY = (y - naughts_y);
-  } else {
+    } else {
       movesX = (x - crosses_x);
       movesY = (y - crosses_y);
+    }
+
+    String turnX = "", turnY = "";
+    if (movesX > 0) {
+      turnX = "e";
+    } else if (movesX < 0) {
+      turnX = "w";
+    }
+    if (movesY > 0) {
+      turnY = "s";
+    } else if (movesY < 0) {
+      turnY = "n";
+    }
+
+    if (turnX == "") {
+
+    } else if (turnY == "") {
+
+    } else {
+
+    }
+
   }
+  void initialise_compass() {
+    // The highest possible magnetic value to read in any direction is 2047
+    // The lowest possible magnetic value to read in any direction is -2047
+    LSM303::vector<int16_t> running_min = { 32767, 32767, 32767 }, running_max = { -32767, -32767, -32767 };
+    unsigned char index;
 
-  String turnX = "", turnY = "";
-  if (movesX > 0) {
-    turnX = "e";
-  } else if (movesX < 0) {
-    turnX = "w";
+    // Initiate the Wire library and join the I2C bus as a master
+    Wire.begin();
+
+    // Initiate LSM303
+    compass.init();
+
+    // Enables accelerometer and magnetometer
+    compass.enableDefault();
+
+    compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 gauss sensitivity to hopefully avoid overflow problems
+    compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ);    // 220 Hz compass update rate
+
+    // To calibrate the magnetometer, the Zumo spins to find the max/min
+    // magnetic vectors. This information is used to correct for offsets
+    // in the magnetometer data.
+    motors.setLeftSpeed(SPEED);
+    motors.setRightSpeed(-SPEED);
+
+    for (index = 0; index < CALIBRATION_SAMPLES; index++)
+    {
+      // Take a reading of the magnetic vector and store it in compass.m
+      compass.read();
+
+      running_min.x = min(running_min.x, compass.m.x);
+      running_min.y = min(running_min.y, compass.m.y);
+
+      running_max.x = max(running_max.x, compass.m.x);
+      running_max.y = max(running_max.y, compass.m.y);
+      delay(50);
+    }
+
+    motors.setLeftSpeed(0);
+    motors.setRightSpeed(0);
+
+    // Set calibrated values to compass.m_max and compass.m_min
+    compass.m_max.x = running_max.x;
+    compass.m_max.y = running_max.y;
+    compass.m_min.x = running_min.x;
+    compass.m_min.y = running_min.y;
   }
-  if (movesY > 0) {
-    turnY = "s";
-  } else if (movesY < 0) {
-    turnY = "n";
-  }
-
-  if (turnX == "") {
-    
-  } else if (turnY == "") {
-    
-  } else {
-    
-  }
-  
-}
-void initialise_compass() {
-  // The highest possible magnetic value to read in any direction is 2047
-  // The lowest possible magnetic value to read in any direction is -2047
-  LSM303::vector<int16_t> running_min = { 32767, 32767, 32767 }, running_max = { -32767, -32767, -32767 };
-  unsigned char index;
-
-  // Initiate the Wire library and join the I2C bus as a master
-  Wire.begin();
-
-  // Initiate LSM303
-  compass.init();
-
-  // Enables accelerometer and magnetometer
-  compass.enableDefault();
-
-  compass.writeReg(LSM303::CRB_REG_M, CRB_REG_M_2_5GAUSS); // +/- 2.5 gauss sensitivity to hopefully avoid overflow problems
-  compass.writeReg(LSM303::CRA_REG_M, CRA_REG_M_220HZ);    // 220 Hz compass update rate
-
-                               // To calibrate the magnetometer, the Zumo spins to find the max/min
-                               // magnetic vectors. This information is used to correct for offsets
-                               // in the magnetometer data.
-  motors.setLeftSpeed(SPEED);
-  motors.setRightSpeed(-SPEED);
-
-  for (index = 0; index < CALIBRATION_SAMPLES; index++)
+  // Average 10 vectors to get a better measurement and help smooth out
+  // the motors' magnetic interference.
+  float averageHeading()
   {
-    // Take a reading of the magnetic vector and store it in compass.m
-    compass.read();
+    LSM303::vector<int32_t> avg = { 0, 0, 0 };
 
-    running_min.x = min(running_min.x, compass.m.x);
-    running_min.y = min(running_min.y, compass.m.y);
+    for (int i = 0; i < 10; i++)
+    {
+      compass.read();
+      avg.x += compass.m.x;
+      avg.y += compass.m.y;
+    }
+    avg.x /= 10.0;
+    avg.y /= 10.0;
 
-    running_max.x = max(running_max.x, compass.m.x);
-    running_max.y = max(running_max.y, compass.m.y);
-    delay(50);
+    // avg is the average measure of the magnetic vector.
+    return heading(avg);
   }
 
-  motors.setLeftSpeed(0);
-  motors.setRightSpeed(0);
-
-  // Set calibrated values to compass.m_max and compass.m_min
-  compass.m_max.x = running_max.x;
-  compass.m_max.y = running_max.y;
-  compass.m_min.x = running_min.x;
-  compass.m_min.y = running_min.y;
-}
-// Average 10 vectors to get a better measurement and help smooth out
-// the motors' magnetic interference.
-float averageHeading()
-{
-  LSM303::vector<int32_t> avg = { 0, 0, 0 };
-
-  for (int i = 0; i < 10; i++)
+  float relativeHeading(float heading_from, float heading_to)
   {
-    compass.read();
-    avg.x += compass.m.x;
-    avg.y += compass.m.y;
+    float relative_heading = heading_to - heading_from;
+
+    // constrain to -180 to 180 degree range
+    if (relative_heading > 180)
+      relative_heading -= 360;
+    if (relative_heading < -180)
+      relative_heading += 360;
+    return relative_heading;
   }
-  avg.x /= 10.0;
-  avg.y /= 10.0;
 
-  // avg is the average measure of the magnetic vector.
-  return heading(avg);
-}
-
-float relativeHeading(float heading_from, float heading_to)
-{
-  float relative_heading = heading_to - heading_from;
-
-  // constrain to -180 to 180 degree range
-  if (relative_heading > 180)
-    relative_heading -= 360;
-  if (relative_heading < -180)
-    relative_heading += 360;
-  return relative_heading;
-}
-
-//this function rotates the zumo based on degrees passed in by the user (i.e. 90)
-//this was heavily inspired from the compass example
-void rotate(int degrees) //rotate to an angle based on compass
-{
-  int speed;
-  float heading = averageHeading();
-
-
-  float relative = relativeHeading(heading, degrees);
-  while (abs(relative) > DEVIATION_THRESHOLD)
+  //this function rotates the zumo based on degrees passed in by the user (i.e. 90)
+  //this was heavily inspired from the compass example
+  void rotate(int degrees) //rotate to an angle based on compass
   {
-    heading = averageHeading();
-    relative = relativeHeading(heading, degrees);
-    speed = SPEED * relative / 180;
-    if (speed < 0)
-      speed -= TURN_BASE_SPEED;
-    else
-      speed += TURN_BASE_SPEED;
-    motors.setSpeeds(speed, -speed);
+    int speed;
+    float heading = averageHeading();
+
+
+    float relative = relativeHeading(heading, degrees);
+    while (abs(relative) > DEVIATION_THRESHOLD)
+    {
+      heading = averageHeading();
+      relative = relativeHeading(heading, degrees);
+      speed = SPEED * relative / 180;
+      if (speed < 0)
+        speed -= TURN_BASE_SPEED;
+      else
+        speed += TURN_BASE_SPEED;
+      motors.setSpeeds(speed, -speed);
+    }
+    motors.setSpeeds(0, 0);
+    delay(400);
   }
-  motors.setSpeeds(0, 0);
-  delay(400);
-}
